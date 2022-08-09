96ba6028cda93276f0c2f191852dd0ec37594d3a
Feng XingYu <50624245+halftheworldaway@users.noreply.github.com>
2022-08-09 17:02:22 +0800

    refactoring shader part

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 ﻿#include <vector>
 
-#include "mesh.hpp"
 #include "renderer.hpp"
-#include "math.hpp"
 #include "shader.hpp"
 
 std::vector<Vertex> vertex_buffer = {
     //坐标                //颜色rgba 
     {{300.0f, 150.0f, 1.0f}, {255, 0, 0, 255}},
-    {{1.0f, 0.0f, 1.0f}, {0, 255, 0, 255}},
-    {{1.0f, 0.0f, 1.0f}, {0, 0, 255, 255}},
+    {{200.0f, 400.0f, 1.0f}, {0, 255, 0, 255}},
+    {{400.0f, 400.0f, 1.0f}, {0, 0, 255, 255}},
 };
 
 std::vector<std::vector<int>> index_buffer = {
@@ -19,5 +17,7 @@ std::vector<std::vector<int>> index_buffer = {
 
 int main(int argc, char* argv[]) {
     Renderer renderer(600, 600);
+    renderer.set_vertexshader(vs_shader);
+    renderer.run(vertex_buffer, index_buffer);
     return 0;
 }
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -1,52 +1,56 @@
 ﻿#pragma once
-#include <SDL.h>
-#include <math.h>
+#include <cmath>
 #include <utility>
 #include <initializer_list>
 #include <algorithm>
+#include <functional>
 
-#include "mesh.hpp"
+#include <SDL.h>
+
 #include "math.hpp"
 
+struct Vertex {
+    Vec3f pos;
+    Vec4i color;
+};
 
 class Renderer {
 private:
     int m_window_width, m_window_height;
     SDL_Renderer* m_renderer;
     SDL_Window* m_window;
+    std::function<Vertex(Vertex)> vertex_shader;
+
 private:
-    void tickrender(const StaticMesh& mesh) {
-        for (auto&& index_seq : mesh.indexbuffer) {
+    void tickrender(const std::vector<Vertex>& vertex_buffer, const std::vector<std::vector<int>>& index_buffer) {
+        for (auto&& index_seq : index_buffer) {
             int index_a = index_seq[0], index_b = index_seq[1], index_c = index_seq[2];
-            draw_primitive(mesh.vertexbuffer[index_a], mesh.vertexbuffer[index_b], mesh.vertexbuffer[index_c]);
+            draw_primitive(vertex_buffer[index_a], vertex_buffer[index_b], vertex_buffer[index_c]);
         }
     }
 
-    void draw_primitive(const std::vector<float>& point_a, const std::vector<float>& point_b, const std::vector<float>& point_c) {
-
-        Vec2f a(point_a[0], point_a[1]); Vec2f b(point_b[0], point_b[1]); Vec2f c(point_c[0], point_c[1]); 
-        Vec3f a_color(point_a[2], point_a[3], point_a[4]), b_color(point_b[2], point_b[3], point_b[4]), c_color(point_c[2], point_c[3], point_c[4]);
-
-        //每个顶点调用一次Vertex shader
+    void draw_primitive(const Vertex& a, const Vertex& b, const Vertex& c) {
         
-        int min_x = std::min({ a.x, b.x, c.x });
-        int min_y = std::min({ a.y, b.y, c.y });
-        int max_x = std::max({ a.x, b.x, c.x });
-        int max_y = std::max({ a.y, b.y, c.y });
+        Vertex a_trans = vertex_shader(a), b_trans = vertex_shader(b), c_trans = vertex_shader(c);
+
+        int min_x = std::min({ a.pos.x, b.pos.x, c.pos.x });
+        int min_y = std::min({ a.pos.y, b.pos.y, c.pos.y });
+        int max_x = std::max({ a.pos.x, b.pos.x, c.pos.x });
+        int max_y = std::max({ a.pos.y, b.pos.y, c.pos.y });
 
         //光栅化部分
         SDL_SetRenderDrawColor(m_renderer, 255, 0, 0, 255);
         for (int x = min_x; x <= max_x; ++x) {
             for (int y = min_y; y <= max_y; ++y) {
                 //用重心坐标来判断当前点是不是在三角形内, https://zhuanlan.zhihu.com/p/65495373参考证明资料
-                Vec3f x_part(b.x - a.x, c.x - a.x, a.x - x);
-                Vec3f y_part(b.y - a.y, c.y - a.y, a.y - y);
+                Vec3f x_part(b_trans.pos.x - a_trans.pos.x, c_trans.pos.x - a_trans.pos.x, a_trans.pos.x - x);
+                Vec3f y_part(b_trans.pos.y - a_trans.pos.y, c_trans.pos.y - a_trans.pos.y, a_trans.pos.y - y);
                 Vec3f u_part = vector_cross(x_part, y_part);
                 Vec3f barycentric(1.0f - (u_part.x + u_part.y) / u_part.z, u_part.x / u_part.z, u_part.y / u_part.z);
                 if (barycentric.x > 0 && barycentric.y > 0 && barycentric.z > 0) {
-                    Uint8 r = static_cast<Uint8>(barycentric.x * a_color.r + barycentric.y * b_color.r + barycentric.z * c_color.r);
-                    Uint8 g = static_cast<Uint8>(barycentric.x * a_color.g + barycentric.y * b_color.g + barycentric.z * c_color.g);
-                    Uint8 b = static_cast<Uint8>(barycentric.x * a_color.b + barycentric.y * b_color.b + barycentric.z * c_color.b);
+                    Uint8 r = static_cast<Uint8>(barycentric.x * a_trans.color.r + barycentric.y * b_trans.color.r + barycentric.z * c_trans.color.r);
+                    Uint8 g = static_cast<Uint8>(barycentric.x * a_trans.color.g + barycentric.y * b_trans.color.g + barycentric.z * c_trans.color.g);
+                    Uint8 b = static_cast<Uint8>(barycentric.x * a_trans.color.b + barycentric.y * b_trans.color.b + barycentric.z * c_trans.color.b);
                     SDL_SetRenderDrawColor(m_renderer, r, g, b, 255);
                     //------每个pixel调用一次fragment_shader------------
                     SDL_RenderDrawPoint(m_renderer, x, y);
@@ -56,11 +60,12 @@ private:
     }
 
 public:
+    Renderer() = delete;
     Renderer(int window_width, int window_height):m_window_width(window_width), m_window_height(window_height) {
         SDL_CreateWindowAndRenderer(window_width, window_height, SDL_WINDOW_RESIZABLE, &m_window, &m_renderer);
     }
 
-    void run(const StaticMesh& mesh) {
+    void run(const std::vector<Vertex>& vertex_buffer, const std::vector<std::vector<int>>& index_buffer) {
         SDL_Event event;
         bool quit = false;
         while (!quit) {
@@ -72,7 +77,7 @@ public:
             //画之前将帧缓存设置为背景颜色。
             SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
             SDL_RenderClear(m_renderer);
-            tickrender(mesh);
+            tickrender(vertex_buffer, index_buffer);
             SDL_RenderPresent(m_renderer);
         }
     }
@@ -81,6 +86,10 @@ public:
         SDL_SetRenderDrawColor(m_renderer, r, g, b, a);
     }
 
+    void set_vertexshader(std::function<Vertex(Vertex)> vs_shader) {
+        vertex_shader = vs_shader;
+    }
+    //析构函数
     ~Renderer() {
         SDL_DestroyWindow(m_window);
         SDL_DestroyRenderer(m_renderer);
