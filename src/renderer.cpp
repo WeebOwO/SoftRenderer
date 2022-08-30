430d751442fa3cf00063fd29948dd74d0757c63b
Feng XingYu <50624245+halftheworldaway@users.noreply.github.com>
2022-08-30 10:46:18 +0800

    light correction

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -4,20 +4,24 @@
 #include "renderer.h"
 
 void Renderer::tickrender(float delta_time, const Buffer& buffer) {
-    static float rotate_speed = 1.0f;
-    static float angle = 0;
+    static float rotate_speed = 0.5f;
+    static float angle = 0.0f;
+    static float move_speed = 10.0f;
+    static float view_offset = 0.0f;
     angle += rotate_speed * delta_time;
-    
-    set_color(0, 0, 0, 255);
+    view_offset += move_speed * delta_time;
     render_clear();
     auto&& vertex_buffer = buffer.verter_buffer;
     auto&& index_buffer = buffer.index_buffer;
+    Mat4x4f offset = matrix_set_translate(0, 0, 2.5);
+    Mat4x4f rotate = matrix_set_rotate(1, 1, 0, angle);
+    Mat4x4f model = rotate * offset;
+    Mat4x4f look_at = matrix_set_lookat({ 0, 0, -1 }, { 0, 0, 0 }, { 0, 1, 0 });
+    Mat4x4f projection = matrix_set_perspective(3.1415926f * 0.35f, 1.0f, 1.0f, 10.0f);
     for (auto&& index : index_buffer) {
-        Mat4x4f offset = matrix_set_translate(0, 0, 2.5);
-        Mat4x4f rotate = matrix_set_rotate(1, 1, 0, angle);
-        Mat4x4f model = rotate * offset;
-        Mat4x4f look_at = matrix_set_lookat({ 0,0,-1 }, { 0,0,0 }, { 0,0,1 });
         global_context::shader_context->set_model_mat(model);
+        global_context::shader_context->set_view_mat(look_at);
+        global_context::shader_context->set_projection_mat(projection);
         draw_primitive(vertex_buffer[index[0]], vertex_buffer[index[1]], vertex_buffer[index[2]]);
     }
     SDL_RenderPresent(m_renderer);
@@ -39,6 +43,7 @@ void Renderer::run(const Buffer& buffer) {
 }
 
 void Renderer::render_clear() {
+    set_color(0, 0, 0, 255);
     SDL_RenderClear(m_renderer);
     for (auto&& depth_vec : m_depth_buffer) {
         std::fill(depth_vec.begin(), depth_vec.end(), 10000.0f);
@@ -63,7 +68,7 @@ void Renderer::rasterize(const Vertex& a_trans, const Vertex& b_trans, const Ver
                 //通过正确的深度来计算插值, 因为屏幕空间下直接利用重心坐标系的结果是不符合在观察坐标系的所看到的深度
                 //其根本原因是，对于深度来说，透视投影的变化对于深度(z)来说并非线性变化，所以投影过后的z值也不能简单的根据深度来进行插值
                 float z = 1 / (barycentric.x * (1 / a_trans.pos.z) + barycentric.y * (1 / b_trans.pos.z) + barycentric.z * (1 / c_trans.pos.z));
-                //进行深度测试
+                //进行深度测试, early-z
                 if (y >= m_depth_buffer.size() || x >= m_depth_buffer[0].size() || m_depth_buffer[y][x] < z) continue;
                 m_depth_buffer[y][x] = z;
                 float a_trans_inverse_z = 1 / a_trans.pos.z, b_trans_inverse_z = 1 / b_trans.pos.z, c_trans_inverse_z = 1 / c_trans.pos.z;
--- src/shader.cpp
+++ src/shader.cpp
@@ -3,26 +3,33 @@
 
 //extern WindowCreaterInfo window_info;
 
+Light light({ 0.0f, 0.0f, 0.0f }, {1.0f, 1.0f, 1.0f});
 Vertex vs_shader(Vertex a) {
-    Light light({ 0.2f, 1.0f, 2.0f }, {1.0f, 1.0f, 1.0f});
     Mat4x4f model = global_context::shader_context->get_model_mat();
     Mat4x4f view = global_context::shader_context->get_view_mat();
-    Mat4x4f normal_model = matrix_invert(model).Transpose();
+    Mat4x4f projection = global_context::shader_context->get_projection_mat();
     Mat4x4f viewport = matrix_set_viewport(600, 600);
-    Mat4x4f projection = matrix_set_perspective(3.1415926f * 0.5f, 1.0f, 1.0f, 500.0f);
-    Vertex temp;
-    Vec4f pos = a.pos.xyz1() * model * projection;
+
     //将法线转化到世界坐标系下
-    //计算漫反射光
-    float diffuse = vector_dot(a.normal, vector_normalize(light.light_pos));
-    diffuse = Max(0.0f, diffuse) + 0.1f;
-    Vec3f color = light.light_color * a.color * diffuse;
+    Mat4x4f normal_model = matrix_invert(model).Transpose();
+
+    Vertex temp;
+    Vec4f pos = a.pos.xyz1() * model * view * projection;
+    a.normal = (a.normal.xyz1() * normal_model).xyz();
+    //计算漫反射光和环境光
+    float diffuse_intensity = Max(vector_dot(a.normal, vector_normalize(light.light_pos - pos.xyz())), 0.f);
+    Vec3f diffuse_color = diffuse_intensity * light.light_color;
+    Vec3f amibinet_color = 0.05f * light.light_color;
+    Vec3f color = (diffuse_color + amibinet_color) * light.light_color;
+    color.r = Min(1.0f, color.r);
+    color.g = Min(1.0f, color.g);
+    color.b = Min(1.0f, color.b);
     //透视除法将齐次坐标转换到欧式空间
     pos /= pos.w;
     //视口转换，将NDC空间的坐标映射到屏幕坐标
     pos = pos * viewport;
     temp.pos = pos.xyz();
-    temp.color = color;
+    temp.color = a.color * color;
     return temp;
 }
 
